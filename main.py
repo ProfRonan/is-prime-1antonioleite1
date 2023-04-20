37d6ce59322fb5de55682e2332be148675928f09
1antonioleite1 <128155482+1antonioleite1@users.noreply.github.com>
2023-04-19 21:42:46 -0300

    feito

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-numero = int(input("Digite um número inteiro positivo"))
+numero = input("digite um número inteiro")
+numero = int(numero)
+a = 1
 
-if numero <= 0:
-    print("Número inválido")
-
+while True:
+    a = a + 1
+    if numero == 1:
+        print("Não primo")
+        break
+    elif numero <= 0:
+        print("Número inválido")
+        break
+    elif numero % a == 0 and a != numero:
+        print("Não primo")
+        break
+    elif numero % a == 0 and a == numero:
+        print("Primo")
+        break
